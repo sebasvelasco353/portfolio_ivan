2c9ea6faed860613b86ba2b7de9f10ff802be65c
sebastian velasco <>
2024-01-20 23:37:54 -0500

    added two sections

--- src/App.jsx
+++ src/App.jsx
@@ -16,18 +16,21 @@ function App() {
             I’m Ivan, a designer specialising in <span className="font-semibold">UI/UX</span> and <span className="font-semibold">Interaction Design</span>
           </p>
         </section>
-        <section className="text-left flex flex-col justify-center">
+        <section id="AboutMe" className="text-left flex flex-col justify-center">
           <div className="content flex flex-col sm:flex-row">
-            <div className="text grow">
-              <h1 className="text-2xl font-semibold">A bit about me</h1>
+            <div className="text grow max-w-screen-sm justify-center flex flex-col">
+              <h1 className="text-2xl pb-4 font-semibold">A bit about me</h1>
               <p className="text-left text-4xl font-light">I am a UI/UX designer & business administrator who is passionate about creating <span className="font-semibold">beautiful and joyful digital experiences. Besides design, I love videogames, digital sculpting and travelling.</span></p>
             </div>
-            <div className="images grow">
-              <img src={imagen1} alt="Ivan on a building" />
-              <img src={imagen2} alt="Ivan with a Pokemon" />
-              <img src={imagen3} alt="Ivan at the stairs of a temple" />
+            <div className="images grow max-w-screen-sm">
+              <img className="image1" src={imagen1} alt="Ivan on a building" />
+              <img className="image2" src={imagen2} alt="Ivan with a Pokemon" />
+              <img className="image3" src={imagen3} alt="Ivan at the stairs of a temple" />
             </div>
           </div>
+        </section>
+        <section id="Work">
+
         </section>
       </main>
       <Footer />
